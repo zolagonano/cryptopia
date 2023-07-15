95f575a3d52d53525ef2edf6368b46da2ef5a1c2
Zola Gonano <zolagonano@protonmail.com>
2023-07-15 11:56:14 +0000

    Change the keygen's structure to allow multi KDFs Support

--- cryptopia-keygen/src/lib.rs
+++ cryptopia-keygen/src/lib.rs
@@ -10,58 +10,89 @@ pub enum WorkFactor {
     Custom(u8, u32, u32),
 }
 
-pub fn keygen(
-    work_factor: &WorkFactor,
-    passphrase: &[u8],
-    salt: &[u8],
-    key_len: u32,
-) -> Result<Vec<u8>, String> {
-    let recommended_params = Params::new(RECOMMENDED_N, RECOMMENDED_R, RECOMMENDED_P);
+impl WorkFactor {
+    fn get_scrypt_params(&self) -> Params {
+        let recommended_params = Params::new(RECOMMENDED_N, RECOMMENDED_R, RECOMMENDED_P);
 
-    let scrypt_params = match work_factor {
-        WorkFactor::Recommended => recommended_params,
-        WorkFactor::Balanced(memory_avail) => {
-            let log_n = (*memory_avail as f64 / 128f64 / RECOMMENDED_R as f64).log2() as u8;
-            if log_n < RECOMMENDED_N {
-                let p = (2u32.pow(RECOMMENDED_N as u32) / 2u32.pow(log_n as u32)) as u32;
-                Params::new(log_n, RECOMMENDED_R, p)
-            } else {
-                recommended_params
+        let scrypt_params = match self {
+            WorkFactor::Recommended => recommended_params,
+            WorkFactor::Balanced(memory_avail) => {
+                let log_n = (*memory_avail as f64 / 128f64 / RECOMMENDED_R as f64).log2() as u8;
+                if log_n < RECOMMENDED_N {
+                    let p = (2u32.pow(RECOMMENDED_N as u32) / 2u32.pow(log_n as u32)) as u32;
+                    Params::new(log_n, RECOMMENDED_R, p)
+                } else {
+                    recommended_params
+                }
             }
+            WorkFactor::Custom(log_n, r, p) => Params::new(*log_n, *r, *p),
+        };
+
+        scrypt_params.unwrap()
+    }
+}
+
+// TODO: Implement new function
+pub struct KeyParams {
+    pub work_factor: WorkFactor,
+    pub passphrase: Vec<u8>,
+    pub salt: Vec<u8>,
+    pub key_len: u32,
+}
+
+pub enum KdfAlgo {
+    Scrypt,
+}
+
+impl KdfAlgo {
+    pub fn keygen(&self, key_params: &KeyParams) -> Result<Vec<u8>, String> {
+        match self {
+            KdfAlgo::Scrypt => Self::keygen_scrypt(&key_params),
         }
-        WorkFactor::Custom(log_n, r, p) => Params::new(*log_n, *r, *p),
-    };
-
-    if let Err(e) = &scrypt_params {
-        return Err(e.to_string());
     }
 
-    let mut buffer = vec![0u8; key_len as usize];
+    fn keygen_scrypt(key_params: &KeyParams) -> Result<Vec<u8>, String> {
+        let scrypt_params = key_params.work_factor.get_scrypt_params();
 
-    let scrypt_result = scrypt(passphrase, salt, &scrypt_params.unwrap(), &mut buffer);
+        let mut buffer = vec![0u8; key_params.key_len as usize];
 
-    if let Err(e) = scrypt_result {
-        return Err(e.to_string());
+        let scrypt_result = scrypt(
+            &key_params.passphrase,
+            &key_params.salt,
+            &scrypt_params,
+            &mut buffer,
+        );
+
+        match scrypt_result {
+            Ok(_) => Ok(buffer),
+            Err(e) => Err(e.to_string()), /*TODO: Use an Enum for Errors*/
+        }
     }
-
-    Ok(buffer)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{keygen, WorkFactor};
+    use super::{KdfAlgo, KeyParams, WorkFactor};
 
     #[test]
     fn balanced() {
-        let passphrase = b"Kubn1xG4ps";
-        let salt = b"AFyIoGyDLJ3Yk2Z6HQKK077524q0E7SOkE6gT4dcd";
+        let passphrase = b"Kubn1xG4ps".to_vec();
+        let salt = b"AFyIoGyDLJ3Yk2Z6HQKK077524q0E7SOkE6gT4dcd".to_vec();
 
         // balanced parameters for 33554432 bytes of available RAM:
         // N = 32768 (2^15)
         // r = 8
         // p = 32
-        let key = keygen(&WorkFactor::Balanced(33554432), passphrase, salt, 10).unwrap();
 
+        let work_factor = WorkFactor::Balanced(33554432);
+        let key_params = KeyParams {
+            work_factor,
+            passphrase,
+            salt,
+            key_len: 10,
+        };
+
+        let key = KdfAlgo::Scrypt.keygen(&key_params).unwrap();
         let expected_result = vec![137, 142, 34, 170, 170, 102, 59, 225, 3, 53];
 
         assert_eq!(expected_result, key);
